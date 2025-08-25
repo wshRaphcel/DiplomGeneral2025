95c888a1b7c785a1a2ec8c0b1ea496a8d7c12b1d
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-08-25 01:47:11 +0000

    adding bootstrap to twig template

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 require('vendor/autoload.php');
 
-use Jm\webproject\App;
+use Jm\Webproject\App;
 
 // create an app object based on App class
 $app = new App();
+// print out the environment variables
+//print_r($_ENV);
+$title = "Home Page";
+$message = "Hello there";
+
+// create a template loader
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+// load the template into memory
+$template = $twig -> load('index.html.twig');
+// add some variables for twig to render
+echo $template -> render([
+    'title' => $title,
+    'message' => $message
+]);
 ?>
--- src/App.php
+++ src/App.php
@@ -1,5 +1,5 @@
 <?php
-namespace Jm\webproject;
+namespace Jm\Webproject;
 // declare other components used in this class
 use Dotenv;
 use \Exception;
@@ -7,7 +7,12 @@ use \Exception;
 class App {
     // visibility
     public function __construct() {
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        // get the root directory or Current Working Directory
+        $path = getcwd();
+        // pass the path to Dotenv to load the .env file
+        $dotenv = Dotenv\Dotenv::createImmutable( $path );
+        // load the .env configuration into $_ENV (super global)
+        $dotenv -> load();
     }
     
 }
