15910b03a0d8c1a7458a8f1cc97f35e6e4127871
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-09-15 02:21:53 +0000

    updated template to display books

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $message = "Hello there";
 $cls_book = new Book();
 $books = $cls_book -> getBooks();
 // check if we have book data
-print_r($books);
+//print_r($books);
 
 // create a template loader
 $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -24,6 +24,7 @@ $template = $twig -> load('index.html.twig');
 // add some variables for twig to render
 echo $template -> render([
     'title' => $title,
-    'message' => $message
+    'message' => $message,
+    'books' => $books
 ]);
 ?>
