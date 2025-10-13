385d0f8fad9f65c84d064a18fda1078e0e497e44
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-10-13 01:22:45 +0000

    added detail

--- detail.php
+++ detail.php
@@ -13,10 +13,21 @@ $message = "Hello there";
 // create an instance of the book class
 $cls_book = new Book();
 
-if( isset($_GET["id"]) ) {
-    echo "<h2>" . $_GET["id"] . "</h2>";
-}
-else {
+if (isset($_GET["id"])) {
+    //echo "<h2>" . $_GET["id"] . "</h2>";
+    // get the book details using its id
+    $book_detail = $cls_book->getBookById($_GET["id"]);
+    // print_r($book_detail);
+    // create a template loader
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+    // load the template into memory
+    $template = $twig->load('detail.html.twig');
+    // add some variables for twig to render
+    echo $template->render([
+        'title' => $book_detail["Title"],
+        'book' => $book_detail
+    ]);
+} else {
     header("location: index.php");
 }
-?>
