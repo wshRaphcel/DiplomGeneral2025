b1c138f82c3acb8375ea1fbee3a8028acf4ee245
Raphcel <lalilah92@gmail.com>
2025-11-03 01:32:32 +0000

    Adding Signup

--- signup.php
+++ signup.php
@@ -7,9 +7,18 @@ use Jm\Webproject\Account;
 // create an app object based on App class
 $app = new App();
 
-$title = "Welcome the the Uni Library";
+$title = "Signup to the Uni Library";
 $message = "Join or community";
 
+if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password1 = $_POST["password"];
+    $password2 = $_POST["confirm-password"];
+
+    $account = new Account();
+    $signup = $account -> create($email,$password,$username,"Test","User");
+}
 
 // create a template loader
 $loader = new \Twig\Loader\FilesystemLoader('templates');
